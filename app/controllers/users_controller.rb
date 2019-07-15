fa1bd55988edddbfed643bfb8aa0aa887566a038
seerosecode <keshalakemusic@gmail.com>
2019-07-15 15:56:30 -0400

    controllers set up. routes not tested yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    this_user
+  end
+
+
+  def update
+    this_user
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    this_user.destroy
+  end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :age, :birthday, :address, :img_url, :family_id, :adult, :parent, :lost, :married)
+end
+
+def this_user
+  @user = User.find(params[:id])
+end
+
 end
--- app/controllers/villages_controller.rb
+++ app/controllers/villages_controller.rb
@@ -1,2 +1,43 @@
 class VillagesController < ApplicationController
+  def index
+    @villages = Village.all
+  end
+
+  def create
+    @village = Village.new(village_params)
+    if village.save
+      render json: @village, status: :created
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    this_village
+  end
+
+  def update
+    this_village
+    if @village.update(village_params)
+      render json: @village
+    else
+      render json: @village.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    this_village.destroy
+  end
+
+  private
+
+  def village_params
+    params.require(:village).permit(:name)
+  end
+
+
+  def this_village
+    @village = Village.find(params[:id])
+  end
+
 end
